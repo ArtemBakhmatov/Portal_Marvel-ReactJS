6816572c7d19394122cfe334fa830e8a753684b1
artem_bakhamtov <ip.bakhmatov@mail.ru>
2023-06-19 19:54:59 +0300

    Formik, Yup - Создание формы. Изменение в маршрутизации

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,16 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
+import { 
+    MainPage, 
+    ComicsPage, 
+    Page404, 
+    SingleComicPage, 
+    SinglePage, 
+    SingleCharacterLayout, 
+    SingleComicLayout 
+} from '../pages';
+
 import AppHeader from "../appHeader/AppHeader";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import Spinner from "../spinner/Spinner";
@@ -10,6 +19,10 @@ const Page404Lazy = lazy(() => import('../pages/404'));         // ленива�
 const MainPageLazy = lazy(() => import('../pages/MainPage'));   
 const ComicsPageLazy = lazy(() => import('../pages/ComicsPage')); 
 const SingleComicPageLazy = lazy(() => import('../pages/SingleComicPage')); 
+const SingleComicLayoutLazy = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayoutLazy = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+const SinglePageLazy = lazy(() => import('../pages/SinglePage'));
+
 
 const App = () =>  {
 
@@ -25,6 +38,8 @@ const App = () =>  {
                             <Route path="/" element={<MainPage/>} />
                             <Route path="/comics" element={<ComicsPage/>} />  
                             <Route path="/comics/:comicId" element={<SingleComicPage/>} />  
+                            {/* <Route exact path="/comics/:comicId" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>} />  */}
+                            <Route exact path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>} />   
                             <Route path="*" element={<Page404/>} />   
                         </Routes>
                     </Suspense>
@@ -35,3 +50,5 @@ const App = () =>  {
 }
 
 export default App;
+
+// 41 строка выдает баг , 40 -ю стороку оставил чтобы вывести отдельного комикса, а так его нужно удалить
